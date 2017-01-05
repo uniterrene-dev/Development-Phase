a51e8ee0ff087126497b871c4c8743a96b6c716c
alokesaha <aloke@uniterrene.com>
2017-01-05 17:58:17 +0530

    chenges main content

--- lavish/wp-content/themes/lavish/home.php
+++ lavish/wp-content/themes/lavish/home.php
@@ -9,12 +9,10 @@ get_header();
     <div class="wrap-center">
             <div class="box1">
                             <h1>High-class escort service - Elite escort agency</h1>
-                        <div class="clear">&nbsp;</div>
-            <p>Since its founding in 2000, our elite escort agency sets new standards across Europe in relation to&nbsp;<a title="VIP escort service" href="escort-service.htm">VIP escort service</a> for the classy gentleman.</p>        </div>
+            <p>Since its founding in 2000, our elite escort agency sets new standards across Europe in relation to&nbsp;<a title="VIP escort service" href="#">VIP escort service</a> for the classy gentleman.</p>        </div>
             <div class="box2">
-                            <h2>Always up-to-date: News about our exclusive escorts </h2>
-                        <div class="clear">&nbsp;</div>
-            <p>In order to find out what is going on in the sensual world of our top escort models check out our <a title="Latest news" href="news.htm">latest news</a>. Updates, photos, information and much more.</p>        </div>
+                            <h2>Always up-to-date: News about our exclusive escorts </h2>        
+            <p>In order to find out what is going on in the sensual world of our top escort models check out our <a title="Latest news" href="#">latest news</a>. Updates, photos, information and much more.</p>        </div>
         </div>
 </div>
 <div class="city-section">
@@ -22,28 +20,61 @@ get_header();
         <h2>Our High Class Escort Service is<br> available in the following cities</h2>
         <p>Target Escorts<sup>&reg;</sup> is an exclusive escort agency that guarantees discretion and reliability.<br> Target Escorts<sup>&reg;</sup> stands for the highest level of service, fun and privacy.<br> Here, gentlemen may expect a premium VIP escort service.</p>
         <div class="city-buttons">
-                        <a href="berlin-escort.htm">Berlin</a>
-                        <a href="cologne-escort.htm">Cologne</a>
-                        <a href="dubai.htm">Dubai</a>
-                        <a href="dusseldorf-escort.htm">Dusseldorf</a>
-                        <a href="frankfurt-escort.htm">Frankfurt</a>
-                        <a href="geneva-escort.htm">Geneva</a>
-                        <a href="hamburg-escort.htm">Hamburg</a>
-                        <a href="london.htm">London</a>
-                        <a href="marbella-escort.htm">Marbella</a>
-                        <a href="milan.htm">Milan</a>
-                        <a href="monaco.htm">Monaco</a>
-                        <a href="munich-escort.htm">Munich</a>
-                        <a href="paris.htm">Paris</a>
-                        <a href="rome.htm">Rome</a>
-                        <a href="saint_tropez.htm">Saint Tropez</a>
-                        <a href="salzburg-escort.htm">Salzburg</a>
-                        <a href="stuttgart-escort.htm">Stuttgart</a>
-                        <a href="vienna-escort.htm">Vienna</a>
-                        <a href="zurich-escort.htm">Zurich</a>
+                        <a href="#">Berlin</a>
+                        <a href="#">Cologne</a>
+                        <a href="#">Dubai</a>
+                        <a href="#">Dusseldorf</a>
+                        <a href="#">Frankfurt</a>
+                        <a href="#">Geneva</a>
+                        <a href="#">Hamburg</a>
+                        <a href="#">London</a>
+                        <a href="#">Marbella</a>
+                        <a href="#">Milan</a>
+                        <a href="#">Monaco</a>
+                        <a href="#">Munich</a>
+                        <a href="#">Paris</a>
+                        <a href="#">Rome</a>
+                        <a href="#">Saint Tropez</a>
+                        <a href="#">Salzburg</a>
+                        <a href="#">Stuttgart</a>
+                        <a href="#">Vienna</a>
+                        <a href="#">Zurich</a>
                     </div>
         <a class="red" id="show-city-list" href="javascript:;">View all cities</a>
     </div>
+</div>
+
+<div class="content-last">
+    <div class="wrap-center mag">
+        <h4>Our Magazine with interesting facts</h4>
+                <div class="blog-box">
+            <div class="blog-thumb">
+                <a href="#">
+                    <img width="1000" height="703" src="http://www.targetescorts.com/magazine/wp-content/uploads/chamonix-escort-service.jpg" class="attachment-medium size-medium wp-post-image" alt="Erotic winter vacation in Chamonix with VIP escort service" srcset="http://www.targetescorts.com/magazine/wp-content/uploads/chamonix-escort-service.jpg 1000w, http://www.targetescorts.com/magazine/wp-content/uploads/chamonix-escort-service-768x540.jpg 768w" sizes="(max-width: 1000px) 100vw, 1000px" />                </a>
+            </div>
+            <h3>Sensual winter magic with VIP escort service Chamonix</h3>
+            <p>Powerful. This is probably the first word that comes to your mind when you admire the highest mountain of the Alps together with your charming escort in...</p>
+            <p><a class="red" href="#" target="_blank">Read More</a></p>
+        </div>
+                <div class="blog-box">
+            <div class="blog-thumb">
+                <a href="#" target="_blank">
+                    <img width="1000" height="600" src="http://www.targetescorts.com/magazine/wp-content/uploads/vierschanzentournee-escort-service.jpg" class="attachment-medium size-medium wp-post-image" alt="Elite escort agency &amp; the Four-Hills-Tournament" srcset="http://www.targetescorts.com/magazine/wp-content/uploads/vierschanzentournee-escort-service.jpg 1000w, http://www.targetescorts.com/magazine/wp-content/uploads/vierschanzentournee-escort-service-768x461.jpg 768w" sizes="(max-width: 1000px) 100vw, 1000px" />                </a>
+            </div>
+            <h3>The Four-Hills-Tournament & our VIP escort agency</h3>
+            <p>A perfect combination for gentlemen who appreciate both the ski jumping as well as the company of gorgeous and stylish escort ladies. Our high-class escort...</p>
+            <p><a class="red" href="#" target="_blank">Read More</a></p>
+        </div>
+                <div class="blog-box">
+            <div class="blog-thumb">
+                <a href="#" target="_blank">
+                    <img width="587" height="311" src="http://www.targetescorts.com/magazine/wp-content/uploads/xmas-service-2016.jpg" class="attachment-medium size-medium wp-post-image" alt="Target Escorts wishes you a festive season!" />                </a>
+            </div>
+            <h3>Merry Christmas & Happy New Year!</h3>
+            <p>The Target Escorts team and all our lovely escort ladies wish you a (sensual) Christmas time! We hope you enjoy the good food, the time with friends and family,...</p>
+            <p><a class="red" href="#" target="_blank">Read More</a></p>
+        </div>
+            </div>
 </div>
 
 
