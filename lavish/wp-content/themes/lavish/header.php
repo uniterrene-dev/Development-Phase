929d1b0766e9d0ed698579b2b1e542817eb3f946
shourya-uniterrene <shourya@uniterrene.com>
2017-01-05 18:21:01 +0530

    Extra Comment lines removed and the video link is added

--- lavish/wp-content/themes/lavish/header.php
+++ lavish/wp-content/themes/lavish/header.php
@@ -31,10 +31,10 @@
 <body>
 <header>
 <div class="top-nav">
-    <div class="wrapper">
+    <div class="container">
         <div class="top-nav">
             <ul>
-                <li><a href="#">MEMBERS LOUNGE</a></li>
+                <li><span> <i class="fa fa-unlock-alt" aria-hidden="true"></i> </span><a href="#">MEMBERS LOUNGE</a></li>
                 <li><a href="#">RATES</a></li>
                 <li><a href="#" id="show-city-list">EXOTIC FANTASIES</a></li>
                 <li><a href="#">NEWS</a></li>
@@ -43,90 +43,7 @@
         </div>
     </div>
 </div>
-<!--<div class="city-menu">
-    <div class="city-wrap">
-                <ul>
-            <li class="c-title">Germany</li>
-                            <li><a href="berlin-escort.htm">Berlin</a></li>
-                            <li><a href="bochum-escort.htm">Bochum</a></li>
-                            <li><a href="bonn-escort.htm">Bonn</a></li>
-                            <li><a href="cologne-escort.htm">Cologne</a></li>
-                            <li><a href="dortmund-escort.htm">Dortmund</a></li>
-                            <li><a href="dresden-escort.htm">Dresden</a></li>
-                            <li><a href="dusseldorf-escort.htm">Dusseldorf</a></li>
-                            <li><a href="essen-escort.htm">Essen</a></li>
-                            <li><a href="frankfurt-escort.htm">Frankfurt</a></li>
-                            <li><a href="hamburg-escort.htm">Hamburg</a></li>
-                            <li><a href="hanover-escort.htm">Hanover</a></li>
-                            <li><a href="karlsruhe-escort.htm">Karlsruhe</a></li>
-                            <li><a href="leipzig-escort.htm">Leipzig</a></li>
-                            <li><a href="mainz-escort.htm">Mainz</a></li>
-                            <li><a href="mannheim-escort.htm">Mannheim</a></li>
-                            <li><a href="munich-escort.htm">Munich</a></li>
-                            <li><a href="nuremberg-escort.htm">Nuremberg</a></li>
-                            <li><a href="stuttgart-escort.htm">Stuttgart</a></li>
-                            <li><a href="wiesbaden-escort.htm">Wiesbaden</a></li>
-                    </ul>
-                <ul>
-            <li class="c-title">Austria</li>
-                            <li><a href="bregenz-escort.htm">Bregenz</a></li>
-                            <li><a href="graz-escort.htm">Graz</a></li>
-                            <li><a href="innsbruck-escort.htm">Innsbruck</a></li>
-                            <li><a href="kitzbuehel-escort.htm">Kitzbühel</a></li>
-                            <li><a href="klagenfurt-escort.htm">Klagenfurt</a></li>
-                            <li><a href="linz-escort.htm">Linz</a></li>
-                            <li><a href="salzburg-escort.htm">Salzburg</a></li>
-                            <li><a href="vienna-escort.htm">Vienna</a></li>
-                            <li><a href="villach-escort.htm">Villach</a></li>
-                            <li><a href="wels-escort.htm">Wels</a></li>
-                    </ul>
-                <ul>
-            <li class="c-title">Switzerland</li>
-                            <li><a href="basel-escort.htm">Basel</a></li>
-                            <li><a href="bern-escort.htm">Bern</a></li>
-                            <li><a href="chur-escort.htm">Chur</a></li>
-                            <li><a href="geneva-escort.htm">Geneva</a></li>
-                            <li><a href="lausanne-escort.htm">Lausanne</a></li>
-                            <li><a href="lucerne-escort.htm">Lucerne</a></li>
-                            <li><a href="st-gallen-escort.htm">St Gallen</a></li>
-                            <li><a href="st-moritz-escort.htm">St Moritz</a></li>
-                            <li><a href="winterthur-escort.htm">Winterthur</a></li>
-                            <li><a href="zurich-escort.htm">Zurich</a></li>
-                    </ul>
-                <ul>
-            <li class="c-title">Spain</li>
-                            <li><a href="barcelona-escort.htm">Barcelona</a></li>
-                            <li><a href="ibiza-escort.htm">Ibiza</a></li>
-                            <li><a href="madrid-escort.htm">Madrid</a></li>
-                            <li><a href="majorca-escort.htm">Majorca</a></li>
-                            <li><a href="marbella-escort.htm">Marbella</a></li>
-                            <li><a href="palma-escort.htm">Palma</a></li>
-                            <li><a href="seville-escort.htm">Seville</a></li>
-                            <li><a href="valencia-escort.htm">Valencia</a></li>
-                    </ul>
-                <ul>
-            <li class="c-title">Benelux</li>
-                            <li><a href="amsterdam-escort.htm">Amsterdam</a></li>
-                            <li><a href="brussels-escort.htm">Brussels</a></li>
-                            <li><a href="luxembourg-escort.htm">Luxembourg</a></li>
-                    </ul>
-                <ul>
-            <li><a href="javascript:;" id="close-city-menu">Close list</a></li>
-        </ul>
-    </div>
-</div>
-<div class="filter_menu">
-    <div class="filter_wrap">
-                    <div class="filter-title">Sensual preferences</div>
-            <ul>
-                                    <li><a href="sensual-preferences/bisexual-escorts.htm">Bisexual Escorts</a></li>
-                                    <li><a href="sensual-preferences/duo-escort-service.htm">Duo Escort Service </a></li>
-                                    <li><a href="sensual-preferences/escorts-couples.htm">Escort Service couples</a></li>
-                                    <li><a href="sensual-preferences/girlfriend-experience.htm">Girlfriend experience</a></li>
-                            </ul>
-                <div class="close-filter_menu"><a href="javascript:;" id="close-filter_menu">Close list</a></div>
-    </div>
-</div>-->
+
 <div class="second-nav">
     <div class="container clearfix">
        <div class="header-logo"> <a href="#"><img src="<?php echo esc_url( get_template_directory_uri() )?>/images/header-logo.png" alt="logo" title="logo image"></a>
@@ -142,40 +59,7 @@
         </div>
     </div>
 </div>
-<!--<div class="lady_menu">
-    <div class="lady_wrap">
-        <div class="all-ladies-link"><strong><a href="#">All Ladies</a></strong></div>
-        <ul>
-                            <li><a href="#">Aaliyah</a></li>
-                            <li><a href="#">Alice</a></li>
-                            <li><a href="#">Alina</a></li>
-                            <li><a href="#">Amelie</a></li>
-                            <li><a href="#">Anna</a></li>
-                            <li><a href="#">Carolina</a></li>
-                            <li><a href="#">Cheryl</a></li>
-                            <li><a href="#">Claudia</a></li>
-                            <li><a href="#">Emily</a></li>
-                            <li><a href="#">Evelyn</a></li>
-                            <li><a href="#">Isabelle</a></li>
-                            <li><a href="#">Jasmin</a></li>
-                            <li><a href="#">Julia</a></li>
-                            <li><a href="#">Kiara</a></li>
-                            <li><a href="#">Laura</a></li>
-                            <li><a href="#">Lina</a></li>
-                            <li><a href="#">Louise</a></li>
-                            <li><a href="#">Mandy</a></li>
-                            <li><a href="#">Mia</a></li>
-                            <li><a href="#">Sandra</a></li>
-                            <li><a href="#">Sasha</a></li>
-                            <li><a href="#">Selina</a></li>
-                            <li><a href="#">Sophie</a></li>
-                            <li><a href="#">Stella</a></li>
-                            <li><a href="#">Victoria</a></li>
-                            <li><a href="#">Violett</a></li>
-                    </ul>
-        <div class="close-lady_menu"><a href="javascript:;" id="close-lady_menu">Close list</a></div>
-    </div>
-</div>-->
+
 </header>
 <div class="index-slide">
     <ul class="target-slider">
@@ -185,9 +69,13 @@
                 </a>
             </li>
             <li>
-                <a href="#">
-                    <img src="<?php echo esc_url( get_template_directory_uri() )?>/images/slider-1.jpg" alt="slider1" title="banner slider">
-                </a>
+               
+				<video width="100%" height="100%" controls autoplay>
+					<source src="<?php echo esc_url( get_template_directory_uri() )?>/video/movie.mp4" type="video/mp4">
+						<source src="<?php echo esc_url( get_template_directory_uri() )?>/video/movie.ogg" type="video/ogg">
+                  
+                </video>
+               
             </li>
             <li>
                 <a href="#">
