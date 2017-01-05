63cfc5b0760b2323c2cd1769e544634a589fc13e
shourya-uniterrene <shourya@uniterrene.com>
2017-01-05 19:11:56 +0530

    New Menu is added as the top-big

--- lavish/wp-content/themes/lavish/functions.php
+++ lavish/wp-content/themes/lavish/functions.php
@@ -58,6 +58,7 @@ function twentyseventeen_setup() {
 	// This theme uses wp_nav_menu() in two locations.
 	register_nav_menus( array(
 		'top'    => __( 'Top Menu', 'twentyseventeen' ),
+		'top-big'    => __( 'Second Menu', 'twentyseventeen' ),
 		'social' => __( 'Social Links Menu', 'twentyseventeen' ),
 	) );
 
