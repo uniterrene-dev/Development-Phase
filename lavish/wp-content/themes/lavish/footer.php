51e004ddd97264754d4f2f8e740cccdde756b737
shourya-uniterrene <shourya@uniterrene.com>
2017-01-05 15:31:21 +0530

    Commit with wp-files

---
+++ lavish/wp-content/themes/lavish/footer.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * The template for displaying the footer
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package WordPress
+ * @subpackage Twenty_Seventeen
+ * @since 1.0
+ * @version 1.0
+ */
+
+?>
+
+<script type="text/javascript" src="<?php echo esc_url( get_template_directory_uri() )?>/js/jquery-1.11.0.js"></script> 
+<script type="text/javascript" src="<?php echo esc_url( get_template_directory_uri() )?>/js/custom.js"></script>
+<script type="text/javascript" src="<?php echo esc_url( get_template_directory_uri() )?>/js/slider.js"></script>
+</body>
+</html>
