ab64d18c2392b7029edaa58fdfa0edc4307926b4
alokesaha <aloke@uniterrene.com>
2017-01-06 18:59:23 +0530

    add footer html

--- lavish/wp-content/themes/lavish/footer.php
+++ lavish/wp-content/themes/lavish/footer.php
@@ -13,7 +13,85 @@
  */
 
 ?>
-
+<footer>
+ <div class="footer-div">
+  <div class="container">
+   <div class="footer-top clearfix">
+    <div class="footer-left">
+     <div class="footer-left-col-1 footer-col">
+      <h5> Get to Know Us </h5>
+      <ul>
+       <li><a href="#">Our Models</a></li>
+       <li><a href="#">About LavishMate</a></li>
+       <li><a href="#">Our Service</a></li>
+       <li><a href="#">Subscribe News Letter </a></li>
+      </ul>
+     </div>
+     <div class="footer-left-col-2 footer-col">
+      <h5> Content </h5>
+      <ul>
+       <li><a href="#">Mate of The Month</a></li>
+       <li><a href="#">Mate of The Year</a></li>
+       <li><a href="#">Gift Ideas</a></li>
+       <li><a href="#">Events</a></li>
+       <li><a href="#">News</a></li>
+       <li><a href="#">Link to Us</a></li>
+      </ul>
+     </div>
+    </div>
+    <div class="footer-middle">
+      <div class="footer-logo">
+       <a href="#"><img src="<?php echo esc_url( get_template_directory_uri() )?>/images/footer-logo.png" / alt=""></a>
+      </div>
+    </div>
+    <div class="footer-right">
+     <div class="footer-left-col-1 footer-col">
+      <h5> Future Lavish Mate </h5>
+      <ul>
+       <li><a href="#">My Calendar</a></li>
+       <li><a href="#">Casting</a></li>
+       <li><a href="#">About Us</a></li>
+       <li><a href="#">The Perfect Lady</a></li>
+       <li><a href="#">Future Model FAQ</a></li>
+      </ul>
+     </div>
+     <div class="footer-left-col-2 footer-col">
+      <h5> Let Us Help You </h5>
+      <ul>
+       <li><a href="#">Members lounge</a></li>
+       <li><a href="#">Rates</a></li>
+       <li><a href="#">Booking</a></li>
+       <li><a href="#">Gentleman FAQ</a></li>
+       <li><a href="#">Our Cities</a></li>
+       <li><a href="#">10 Steps to Conquer</a></li>
+       <li><a href="#">White Us a Feedback</a></li>
+      </ul>
+     </div>
+    </div>
+   </div> 
+   <div class="footer-bottom">
+     <p> Payment options </p>
+     <p>We accept the following credit cards: </p>
+     <div class="payment-div"> 
+       <a href="#"><img src="<?php echo esc_url( get_template_directory_uri() )?>/images/credit_grey.png" / alt=""></a>
+     </div>
+   </div>
+   <div class="copy-right-div clearfix">
+    <div class="copy-right-left">
+      <p> © 2016 Lavish Mate®  Entertainment- All rights reserved  <span>Privacy Policy, Terms of Use, Copy Right Policy </span> </p>
+    </div>
+    <div class="copy-right-right">
+     <ul>
+      <li> <a href="#"> <i class="fa fa-facebook" aria-hidden="true"></i> </a> </li>
+      <li> <a href="#"> <i class="fa fa-twitter" aria-hidden="true"></i> </a> </li>
+      <li> <a href="#"> <i class="fa fa-instagram" aria-hidden="true"></i> </a> </li>
+      <li> <a href="#"> <i class="fa fa-youtube" aria-hidden="true"></i> </a> </li>
+     </ul>
+    </div>
+   </div>
+  </div>
+ </div>
+</footer>
 
 
 <script type="text/javascript" src="<?php echo esc_url( get_template_directory_uri() )?>/js/jquery-1.11.0.js"></script> 
