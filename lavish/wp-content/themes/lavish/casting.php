63625fcfbcabe3345514531531f59f941f6d0dc9
alokesaha <aloke@uniterrene.com>
2017-01-07 15:17:24 +0530

    Casting html

--- lavish/wp-content/themes/lavish/casting.php
+++ lavish/wp-content/themes/lavish/casting.php
@@ -4,7 +4,68 @@
  */
 get_header('casting');
 ?>
-
+<section id="casting-about" class="casting-about-div">
+ <div class="container">
+  <div class="casting-about-top">
+   <ul>
+    <li><a href="javascript:void(0)">ABOUT US</a></li>
+    <li><a href="javascript:void(0)">THE PERFECT LADY</a></li>
+    <li><a href="javascript:void(0)">FUTURE MODEL FAQ</a></li>
+    <li><a href="javascript:void(0)">CONTACT US</a></li>
+   </ul>
+   <div class="about-box-divider">
+    <span> </span>
+   </div>
+  </div>
+  <div class="casting-about-bottom">
+   <h3> Cast Your Lavish Mate® </br> Model Job</h3>
+  <div class="casting-about-bottom-content"> 
+   <p> The need for reservations is very high at Lavish Mate®. Since we work with escort models that are well established in their personal and professional life, it is important that they be informed 
+beforehand of any date/ travel engagement that you might be looking forward to. In our exclusive VIP member lounge we provide pictures of our charming ladies where you 
+may admire also their beautiful faces; this helps you to choose one of the fascinating models for an unforgettable date. </p>
+   <p> The beautiful and alluring Lavish Mate ® models also travel companions love to escort you somewhere new, and always enjoy a chance to venture out of their locale and savor an extended travel 
+assignment with you. Whether you're booking 6 hours for a coast-to-coast business trip, or a multi-day indulgence, we can definitely assist you. Perhaps you're seeking a revitalizing 
+weekend away, or a relaxing vacation for a few weeks? Regardless of your timing, we'll do our best to accommodate your schedule and preferences for companionship. Your time 
+will be fondly remembered.As soon as you become a client of our high-class escort agency we offer you free access promptly and smoothly, have a nice time in the member lounge. </p>
+  </div>
+  </div>
+ </div>
+</section>
+<section id="casting-amazing" class="casting-amazing-div">
+ <div class="container">
+  <div class="casting-amazing-top clearfix">
+   <div class="about-box-divider"> <span> </span> </div>
+   <div class="casting-amazing-top-left">
+    <div class="casting-amazing-top-heading">
+     <h3> They’re All So Amazing; How Do You Make Your Mind Up? </h3>
+    </div>
+    <div class="casting-amazing-top-contant">
+     <p> We know, we’ve probably made it challenging for  you to choose the cream 
+of the crop from our selection of companions. If you need a little extra help, 
+you can check out feedback from other satisfied clients. If you would like an 
+insider’s opinion, please <strong>contact us</strong> for advice. We look forward to being 
+of service and welcoming you to Lavish Mate®. </p>
+    </div>
+   </div>
+   <div class="casting-amazing-top-right">
+    <div class="casting-amazing-top-heading">
+     <h3> They’re All So Amazing; How Do You Make Your Mind Up? </h3>
+    </div>
+    <div class="casting-amazing-top-contant">
+     <p> We know, we’ve probably made it challenging for  you to choose the cream 
+of the crop from our selection of companions. If you need a little extra help, 
+you can check out feedback from other satisfied clients. If you would like an 
+insider’s opinion, please <strong>contact us</strong> for advice. We look forward to being 
+of service and welcoming you to Lavish Mate®. </p>
+    </div>
+   </div>
+  </div>
+  <div class="casting-amazing-bottom">
+    <p> We would like to thank you in advance for your trust and look forward to answering your booking inquiry immediately and discreetly. </p>
+    <p> Please note that online booking requires 2 hours in advance in order to process for our operators. If you would like to let us know any of your wishes please use the required forms attached. </p>
+  </div>
+ </div>
+</section>
 <?php
 get_footer();
 ?>
