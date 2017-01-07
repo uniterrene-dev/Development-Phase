46b153e0562ddb59c21786997f07d391e1b55334
alokesaha <aloke@uniterrene.com>
2017-01-07 18:36:36 +0530

    change js

--- lavish/wp-content/themes/lavish/js/custom.js
+++ lavish/wp-content/themes/lavish/js/custom.js
@@ -1,7 +1,7 @@
 //casting from js 
 jQuery('.casting-tab-name').click(function(e) {	
 	jQuery('.casting-tab-details').hide();
-    var ids = jQuery(this).attr('href');
+    var ids = jQuery(this).attr('data-href');
 	jQuery(ids).show();
 });
 
