c026b4102dca787cf14c53dd0ea77cbe16e28e9b
alokesaha <aloke@uniterrene.com>
2017-01-09 15:09:27 +0530

    Change video js

--- lavish/wp-content/themes/lavish/js/custom.js
+++ lavish/wp-content/themes/lavish/js/custom.js
@@ -8,3 +8,29 @@ jQuery('.casting-tab-name').click(function(e) {
  
 //casting from js end //
 
+var vid = document.getElementById("bgvid");
+var pauseButton = document.querySelector("#");
+
+function vidFade() {
+  vid.classList.add("stopfade");
+}
+
+vid.addEventListener('ended', function()
+{
+// only functional if "loop" is removed 
+vid.pause();
+// to capture IE10
+vidFade();
+}); 
+
+
+//pauseButton.addEventListener("click", function() {
+  //vid.classList.toggle("stopfade");
+  //if (vid.paused) {
+    //vid.play();
+    //pauseButton.innerHTML = "Pause";
+  //} else {
+    //vid.pause();
+    //pauseButton.innerHTML = "Paused";
+  //}
+//})
