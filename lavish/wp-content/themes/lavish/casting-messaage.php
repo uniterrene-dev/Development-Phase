e6566ca80efe0c2f1ce246c5d95ede88cc7acf10
alokesaha <aloke@uniterrene.com>
2017-01-09 18:58:45 +0530

    casting message

---
+++ lavish/wp-content/themes/lavish/casting-messaage.php
@@ -0,0 +1,66 @@
+<?php
+/**
+ * Template Name: message Page
+ */
+get_header('message');
+?>
+<section id="hello-dear" class="hello-dear-div">
+  <div class="container">
+    <div class="hello-dear-box">
+      <h3> Hello Dear, </h3>
+      <p> Yes, we know — <strong>nobody</strong> likes automated messages, but we just wanted to let you know that your 
+application to join our agency position was submitted successful. Thank you. </p>
+      <p> We look forward to learning more about you and whether the Lavish Mate Agency might be the right fit. If you happen to match up with our agency’s high demands, we'll be in touch to set up a meeting. </p>
+    </div>
+  </div>
+</section>
+<section id="application-details" class="application-details-div">
+ <div class="container">
+  <div class="application-details-box">
+   <div class="application-details-heading">
+    <h1> applicant details </h1>
+   </div> 
+   <div class="application-details">
+    <div class="application-detail-top">
+     <p> ou'll be thrilled to know that our office is looking in to your application for the next open interview. </p>
+     <p> This glamorous, happy life can be yours, working with one of the most prestigious, premium introduction 
+agencies in the world. To be considered as part of our ‘family’, you must be very fit, 
+healthy and naturally beautiful. You must also be a warm-hearted and friendly person, 
+with a humble soul and a loving nature. </p>
+     <p> Lavish Mate® offers a genuinely wonderful opportunity for intelligent, beautiful women to 
+supplement their income with a part-time luxurious position. Enjoy an excellent income, 
+and meet interesting, lovely new people in the process. </p>
+     <div class="application-details-heading">
+       <h1> good to know </h1>
+     </div>
+    </div> 
+    <div class="application-detail-bottom">
+     <p> We guarantee your information is 100% confidential forever, and is never shared with anyone outside of the agency management, for any reason. </p>
+    <div class="application-detail-bottom-bottom"> 
+     <p> We take pride of our discretions, we promise our 100% discretion and confidentiality. All our staff sign legal non-disclosure agreements, 
+and we get your word to NEVER disclose the details of our clients, ever. How common, to stoop so low as to use someone 
+else's trust in one, to gain notoriety or income! </p>
+    </div>
+    <div class="application-detail-bottom-footer">
+     <p> n the meantime – please learn more about our AGENCY at www.lavishmate.com 
+Thanks again for taking the leap with us — we hope to speak with you soon. </p>
+    <div class="application-detail-bottom-footer-bottom">
+     <p> We do not confirm the booking price or charge your card until your date end, final price will be confirm when the escort model call our office to confirm the exact final time the date 
+ended. This escort companion booking acknowledgement email simply means that your booking request has been received; it does not mean that your booking has been accepted for  the booking date has picked, is the model ready to dispatch right away, or that the price or availability of the model has been confirmed </p>
+     <p> This email was sent from a notification-only address that cannot accept incoming email. Please do not reply to this message. </p>
+    </div>
+    </div>
+    
+    <div class="application-detail-footer">
+      <p> © 2016 lavishmate.com. Lavish Mate  </p>
+      <p> are registered trademarks of Lavish Mate. All rights reserved. Miami — Boca Raton  — United State —  International — Toll Free —1-866-662-3669  </p>
+    </div>
+     
+    </div>
+   </div>
+  </div>
+ </div>
+</section>
+
+
+
