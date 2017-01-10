ca8a414d2ac8811921a6200c30d3d98d0ed7d734
shourya-uniterrene <shourya@uniterrene.com>
2017-01-10 12:02:28 +0530

    New home page template

--- lavish/wp-content/themes/lavish/front-page.php
+++ lavish/wp-content/themes/lavish/front-page.php
@@ -1,54 +1,206 @@
 <?php
 /**
- * The front page template file
- *
- * If the user has selected a static page for their homepage, this is what will
- * appear.
- * Learn more: https://codex.wordpress.org/Template_Hierarchy
- *
- * @package WordPress
- * @subpackage Twenty_Seventeen
- * @since 1.0
- * @version 1.0
+ * Template Name: Home Page
  */
+get_header();
+?>
 
-get_header(); ?>
+<section id="about-box" class="about-div">
+ <div class="container clearfix">
+  <div class="home-about-box">
+   <div class="home-about-col-1">
+     <h3>  <?php echo get_option('webq_es_srv');?> </h3>
+     <p><?php echo get_option('webq_es_srv_des');?></p>
+   </div>
+   <div class="home-about-col-2">
+     <h3>  <?php echo get_option('webq_es_srv2');?> </h3>
+     <p><?php echo get_option('webq_es_srv_des2');?></p>
+   </div>
+   <div class="home-about-col-3">
+     <h3>  <?php echo get_option('webq_es_srv3');?> </h3>
+     <p><?php echo get_option('webq_es_srv_des3');?></p>
+   </div>
+ </div>
+  <div class="about-box-divider"><span> </span></div>
+</section>
+<section id="home-model" class="home-model-div">
+ <div class="container">
+   <div class="home-model-box clearfix">
+    <div class="home-model-box-left">
+      <div class="home-model-box-img">
+       <a href="#"><img src="<?php echo esc_url( get_template_directory_uri() )?>/images/model-home-1.jpg" /></a>
+      </div>
+      <div class="home-model-box-content">
+      <h3> <a href="#">Yvonno</a> </h3>
+      <h4> Sexy Blonde </h4>
+      <div class="home-model-box-details">
+        <a href="#"> View this model <span> <i class="fa fa-angle-right" aria-hidden="true"></i> </span></a>
+      </div> 
+    </div>
+    </div>
+    <div class="home-model-box-right">
+      <div class="home-model-box-img">
+       <a href="#"><img src="<?php echo esc_url( get_template_directory_uri() )?>/images/model-home-2.jpg" / alt=""></a>
+      </div>
+      <div class="home-model-box-content">
+      <h3> <a href="#">Angelina</a> </h3>
+      <h4> Cute Blonde </h4>
+      <div class="home-model-box-details">
+        <a href="#"> View this model <span> <i class="fa fa-angle-right" aria-hidden="true"></i> </span></a>
+      </div> 
+    </div>
+    </div>
+    
+   </div>
+ </div>
+</section>
+<section id="model-view" class="model-view-div">
+ <div class="container">
+  <div class="model-view-box">
+   <ul>
+    <li>
+     <div class="model-view-img">
+      <a href="#"><img src="<?php echo esc_url( get_template_directory_uri() )?>/images/model-view-1.jpg" /></a>
+     </div>
+     <div class="model-view-content">
+      <h3> <a href="#">Kristina</a> </h3>
+      <h4> Sweet & Sexy </h4>
+      <div class="home-model-box-details">
+        <a href="#"> View this model <span> <i class="fa fa-angle-right" aria-hidden="true"></i> </span></a>
+      </div>
+     </div>
+    </li>
+    <li>
+     <div class="model-view-img">
+      <a href="#"><img src="<?php echo esc_url( get_template_directory_uri() )?>/images/model-view-2.jpg" / alt=""></a>
+     </div>
+     <div class="model-view-content">
+      <h3> <a href="#">Marie</a> </h3>
+      <h4> Smart and Sexy model </h4>
+      <div class="home-model-box-details">
+        <a href="#"> View this model <span> <i class="fa fa-angle-right" aria-hidden="true"></i> </span></a>
+      </div>
+     </div>
+    </li>
+    <li>
+     <div class="model-view-img">
+      <a href="#"><img src="<?php echo esc_url( get_template_directory_uri() )?>/images/model-view-3.jpg" / alt=""></a>
+     </div>
+     <div class="model-view-content">
+      <h3> <a href="#">Jade</a> </h3>
+      <h4> Busty & Classy </h4>
+      <div class="home-model-box-details">
+        <a href="#"> View this model <span> <i class="fa fa-angle-right" aria-hidden="true"></i> </span></a>
+      </div>
+     </div>
+    </li>
+   </ul>
+  </div>
+ </div>
+</section>
 
-<div id="primary" class="content-area">
-	<main id="main" class="site-main" role="main">
+<section id="city-section" class="city-section-div">
+    <div class="container">
+       <div class="city-details clearfix">
+         <div class="city-details-left">
+           <h3> <?php echo get_option('webq_dis_srv');?> </h3>
+           <p> <?php echo get_option('webq_dis_srv_des');?> </p>
+         </div>
+         <div class="city-details-right">
+           <h3> <?php echo get_option('webq_dis_srv2');?> </h3>
+           <p> <?php echo get_option('webq_dis_srv_des2');?> </p>
+         </div>
+       </div> 
+        <div class="city-buttons">
+                       <ul> 
+						 
+<?php 
+						   $args = array('child_of' => '4');
+$categories = get_categories( $args );
+foreach($categories as $category) { 
+   // print_r($category);
+    
+    echo ' <li><a href="' . get_category_link( $category->term_id ) . '">'. $category->name.'</a></li>';
+} ?>
 
-		<?php // Show the selected frontpage content.
-		if ( have_posts() ) :
-			while ( have_posts() ) : the_post();
-				get_template_part( 'template-parts/page/content', 'front-page' );
-			endwhile;
-		else : // I'm not sure it's possible to have no posts when this page is shown, but WTH.
-			get_template_part( 'template-parts/post/content', 'none' );
-		endif; ?>
 
-		<?php
-		// Get each of our panels and show the post data.
-		if ( 0 !== twentyseventeen_panel_count() || is_customize_preview() ) : // If we have pages to show.
+                       </ul> 
+                    </div>
+        <div class="city-more-btn-div">            
+         <a class="city-more-btn" id="show-city-list" href="javascript:;">View All Lavish Mate Cities</a>
+        </div> 
+    </div>
+</section> <!----- city name section -->
 
-			/**
-			 * Filter number of front page sections in Twenty Seventeen.
-			 *
-			 * @since Twenty Seventeen 1.0
-			 *
-			 * @param $num_sections integer
-			 */
-			$num_sections = apply_filters( 'twentyseventeen_front_page_sections', 4 );
-			global $twentyseventeencounter;
+<section id="exclusive-models" class="exclusive-models-div">
+  <div class="container">
+    <div class="exclusive-models-box">
+<!--
+       <a href="#"> View All Exclusive Models </a>
+-->
+<?php echo get_option('webq_ex_models');?>
+    </div>
+  </div>
+</section> <!----- Exclusive Models -->
 
-			// Create a setting and control for each of the sections available in the theme.
-			for ( $i = 1; $i < ( 1 + $num_sections ); $i++ ) {
-				$twentyseventeencounter = $i;
-				twentyseventeen_front_page_section( null, $i );
-			}
+<section id="latest-news-section" class="latest-news-div">
+ <div class="container">
+  <div class="latest-news-header">
+    <div class="latest-news-heading"> <h3><span class="latest-head"><?php echo get_option('webq_lat_news');?></span></h3> </div>
+    <div class="latest-news-sub-heading"><p> <?php echo get_option('webq_lat_news_sub_heading');?></p> </div>
+  </div>
+  <div class="latest-news-box clearfix">
+<?php
+$args = array(
+	'numberposts' => 3,
+	'offset' => 0,
+	'category' => 28,
+	'orderby' => 'rand',
+	'order' => 'ASC',
+	'include' => '',
+	'exclude' => '',
+	'meta_key' => '',
+	'meta_value' =>'',
+	'post_type' => 'post',
+	'post_status' => 'draft, publish, future, pending, private',
+	'suppress_filters' => true
+);
 
-	endif; // The if ( 0 !== twentyseventeen_panel_count() ) ends here. ?>
+$recent_posts = wp_get_recent_posts( $args, ARRAY_A );
+$j=1;
+foreach($recent_posts as $recent_posts)
+{ 
+	//print_r($recent_posts);
 
-	</main><!-- #main -->
-</div><!-- #primary -->
+$class_name = "latest-news-col-".$j;	
+$image = wp_get_attachment_image_src( get_post_thumbnail_id( $recent_posts['ID'] ), 'single-post-thumbnail' ); 
+$post_content = $recent_posts['post_content'];
+$post_title = $recent_posts['post_title'];
+$guid = $recent_posts['guid'];
+?>
 
-<?php get_footer();
+ 
+   <div class="<?php echo $class_name;?> latest-news">
+     <div class="latest-news-img">
+       <a href="#"><img src="<?php echo $image[0];?>" / alt=""></a>
+     </div>
+     <div class="latest-news-content">
+       <h2><a href="<?php echo $guid;?>"> <?php echo $post_title; ?> </a></h2>
+       <h4> <?php echo get_the_time('F j, Y', $recent_posts['ID']);?> </h4>
+       <p> <?php echo $post_content; ?></p>
+       <div class="read-btn">
+        <a href="<?php echo $guid;?>"> Read article <span> <i class="fa fa-angle-right" aria-hidden="true"></i> </span></a>
+       </div>
+     </div>
+   </div>
+   <?php $j++; } ?>	 
+
+  </div>
+  <div class="copyscape-btn">
+    <a href="#"><img src="<?php echo esc_url( get_template_directory_uri() )?>/images/copyscape-banner-gray.png" / alt=""></a>
+  </div>
+ </div>
+</section> <!----- Latest News -->
+
+
+<?php get_footer();?>
