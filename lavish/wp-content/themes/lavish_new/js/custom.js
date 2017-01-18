2242a33adf623146ff9c13abf012c85530f0cb37
alokesaha <aloke@uniterrene.com>
2017-01-18 16:10:00 +0530

    Change js

--- lavish/wp-content/themes/lavish_new/js/custom.js
+++ lavish/wp-content/themes/lavish_new/js/custom.js
@@ -8,9 +8,10 @@ jQuery('.casting-tab-name').click(function(e) {
 
 // ***** vip accordion ***** // 
 (function($) {
-    $('.accordion > li:eq(0) .accordion-tab').addClass('active');
+    $('.accordion > li:eq(0) .accordion-tab').addClass('');
 	
-	if ( $( '.accordion > li:eq(0) .accordion-tab' ).hasClass( "active" ) ) {
+	//if ( $( '.accordion > li:eq(0) .accordion-tab' ).hasClass( "active" ) ) {
+		if ( $( '.accordion > li:eq(0) .accordion-tab' ).hasClass( "active" ) ) {
 	//$('.accordian-active p').show();
 	$('.accordian-active.accordion-panel').show();
 	}
@@ -37,3 +38,43 @@ jQuery('.casting-tab-name').click(function(e) {
     });
 })(jQuery);
 // ***** vip accordion end***** // 
+
+
+// multiselect //
+
+$(".dropdown dt a").on('click', function() {
+  $(".dropdown dd ul").slideToggle('fast');
+});
+
+$(".dropdown dd ul li a").on('click', function() {
+  $(".dropdown dd ul").hide();
+});
+
+function getSelectedValue(id) {
+  return $("#" + id).find("dt a span.value").html();
+}
+
+$(document).bind('click', function(e) {
+  var $clicked = $(e.target);
+  if (!$clicked.parents().hasClass("dropdown")) $(".dropdown dd ul").hide();
+});
+
+$('.mutliSelect input[type="checkbox"]').on('click', function() {
+
+  var title = $(this).closest('.mutliSelect').find('input[type="checkbox"]').val(),
+    title = $(this).val() + ",";
+
+  if ($(this).is(':checked')) {
+    var html = '<span title="' + title + '">' + title + '</span>';
+    $('.multiSel').append(html);
+    $(".hida").hide();
+  } else {
+    $('span[title="' + title + '"]').remove();
+    var ret = $(".hida");
+    $('.dropdown dt a').append(ret);
+
+  }
+});
+
+
+
