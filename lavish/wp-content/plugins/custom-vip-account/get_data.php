116ad1507f7a3b7b25034215b4edf04c5fa2c9f3
shourya-uniterrene <shourya@uniterrene.com>
2017-01-24 18:19:02 +0530

    remove the extra drop down

--- lavish/wp-content/plugins/custom-vip-account/get_data.php
+++ lavish/wp-content/plugins/custom-vip-account/get_data.php
@@ -108,16 +108,7 @@ function registration_form()
              <option name="evening" value="evening">evening </option>
            </select> 
         </div>
-
-         <div class="vip-fields"> 
-           <select name="total_plan" id="total_plans">
-             <option name="morning" value="morning">morning</option>
-             <option name="afternoon" value="afternoon">afternoon</option>
-             <option name="evening" value="evening">evening </option>
-           </select> 
-        </div>
-
-       </li>
+		</li>
        <li class="vip-form-box-field">
         <div class="vip-label"> <label> If wish not be contacted </label> </div>
         <div class="vip-fields">
