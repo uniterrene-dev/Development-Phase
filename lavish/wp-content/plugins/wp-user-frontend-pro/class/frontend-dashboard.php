4fd148d6d72954be3e84512545c0529a1cf1fa53
shourya-uniterrene <shourya@uniterrene.com>
2017-01-31 17:51:03 +0530

    Added the custom tab form

--- lavish/wp-content/plugins/wp-user-frontend-pro/class/frontend-dashboard.php
+++ lavish/wp-content/plugins/wp-user-frontend-pro/class/frontend-dashboard.php
@@ -101,80 +101,202 @@ class WPUF_Frontend_Dashboard {
 
         if ( wpuf_get_option( 'show_user_bio', 'wpuf_dashboard', 'on' ) == 'on' ) {
             ?>
-            <div class="dashboard-get_option">
-             <div class="container">
-              <div class="wpuf-author">
-                <h3><?php _e( 'Author Info', 'wpuf' ); ?></h3>
-                <div class="wpuf-author-inside odd">
-                    <div class="wpuf-user-image"><?php echo get_avatar( $userdata->user_email, 80 ); ?></div>
-                    <div class="wpuf-author-body">
-                        <p class="wpuf-user-name"><a href="<?php echo get_author_posts_url( $userdata->ID ); ?>"><?php printf( esc_attr__( '%s', 'wpuf' ), $userdata->display_name ); ?></a></p>
-                        <p class="wpuf-author-info">
-						  <?php //echo $userdata->description; ?>
-                          
-                        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. </p>
-                    </div>
-                </div>
-                
-                <div class="dashboard-box">
-                  <table class="rwd-table">
+             <link rel="stylesheet" type="text/css"  href="<?php echo esc_url( get_template_directory_uri() )?>/css/vertical-tab.css">
+
+<section id="members-dashboard" class="members-dashboard-div">
+   <div class="members-dashboard-heading-tag">
+     <div class="container">
+       <ul>
+        <li>Book a new model</li>
+        <li>My VIP plan</li>
+        <li>My upcoming events</li>
+       </ul>
+     </div>  
+   </div>
+   <div class="container">
+     <div class="members-dashboard-box clearfix">
+       
+     <div class="vertical-tab">
+     <ul class="tabs">
+      <li class="vertical-panel active" rel="tab1">My Info <span> <img src="<?php echo esc_url( get_template_directory_uri() )?>/images/dashboard-icon-1.png" alt=""> </span></li>
+      <li class="vertical-panel" rel="tab2">Previous History <span> <img src="<?php echo esc_url( get_template_directory_uri() )?>/images/dashboard-icon-2.png" alt=""> </span></li>
+      <li class="vertical-panel" rel="tab3">Payment Methods<span> <img src="<?php echo esc_url( get_template_directory_uri() )?>/images/dashboard-icon-3.png" alt=""> </span></li>
+     </ul>
+    <div class="tab_container">
+      <div class="d_active tab_drawer_heading" rel="tab1">
+       <h3> My Info </h3>
+      </div>
+      
+      <div id="tab1" class="tab_content">
+       <div class="payment-method-box clearfix">
+        <div class="payment-method-client">
+          <img src="<?php echo esc_url( get_template_directory_uri() )?>/images/dashboard-client.png" alt="">
+        </div>
+        <div class="payment-method-client-content">
+			<?php
+			
+			//print_r($userdata);
+			?>
+         <h3> <?php printf( esc_attr__( '%s', 'wpuf' ), $userdata->display_name ); ?> </h3>
+         <h4> <?php printf( esc_attr__( '%s', 'wpuf' ), $userdata->user_nicename ); ?> </h4>
+        </div> 
+       </div>
+       <div class="billing-payment-box">
+       <h3> Account Details </h3>
+      </div>
+       <div class="payment-info-box">
+        <table class="rwd-table" cellpadding="0" cellspacing="0">
+          <tr class="payment-info-box-top">
+            <th>Client name/User name</th>
+            <th>Address</th>
+            <th>Phone</th>
+            <th>Email</th>
+          </tr>
+          <tr>
+            <td data-th="Client name/User name"> <strong><?php printf( esc_attr__( '%s', 'wpuf' ), $userdata->display_name ); ?> </br>
+<?php printf( esc_attr__( '%s', 'wpuf' ), $userdata->user_nicename ); ?></strong></td>
+            <td data-th="Address">
+<?php echo esc_attr( get_the_author_meta( 'address', $userdata->ID ) ); ?></td>
+            <td data-th="Phone"><?php echo esc_attr( get_the_author_meta( 'phone', $userdata->ID ) ); ?></br>
+                <?php echo esc_attr( get_the_author_meta( 'mobphone', $userdata->ID ) ); ?> </td>
+            <td data-th="Email" class="payment-email"> <?php printf( esc_attr__( '%s', 'wpuf' ), $userdata->user_email ); ?> </td>
+          </tr>
+          <tr class="payment-info-box-bottom">
+            <td data-th="Client name/User name"> </td>
+            <td data-th="Address"><a href="#">Edit</a></td>
+            <td data-th="Phone"><a href="#">Edit</a></td>
+            <td data-th="Email"><a href="#">Edit</a></td>
+          </tr>
+        </table>
+        
+        <div class="Back-to-Escort-Ladies-btn">
+          <a href="#"> Back to Escort Ladies </a>
+        </div>
+        
+      </div>
+       
+      </div>
+  <!-- #tab1 -->
+  <div class="tab_drawer_heading" rel="tab2"><h3>Previous History </h3></div>
+  <div id="tab2" class="tab_content">
+    <div class="dashboard-get_option">
+      <div class="dashboard-box">
+		  <table class="rwd-table dashboard-member-details" cellpadding="0" cellspacing="0" width="100%">
                       <tr class="odd">
-                        <th>Dress Style</th>
+                        <th>Model</th>
+                        <th>Dress style</th>
                         <th>Location</th>
-                        <th>Hotel</th>
+                        <th>Hotel#</th>
                         <th>Date of meeting</th>
-                        <th>Time of Date</th>
+                        <th>Time of meeting</th>
                         <th>Duration</th>
-                        <th>Duration</th>
-                        <th>Client Message</th>
+                        <th>Message</th>
                       </tr>
-                      <tr class="even">
-                        <td data-th="Dress Style">Sporty Chic</td>
-                        <td data-th="Location">Hotel Visit</td>
-                        <td data-th="Hotel"></td>
-                        <td data-th="Date of meeting">2017-1-24</td>
-                        <td data-th="Time of Date">3</td>
-                        <td data-th="Duration">1 Hours</td>
-                        <td data-th="Duration">Name:Test name</br> Age: 28</td>
-                        <td data-th="Client Message"></td>
-                      </tr>
-                      <tr class="odd">
-                        <td data-th="Dress Style">Sporty Chic</td>
-                        <td data-th="Location">Hotel Visit</td>
-                        <td data-th="Hotel"></td>
-                        <td data-th="Date of meeting">2017-1-24</td>
-                        <td data-th="Time of Date">3</td>
-                        <td data-th="Duration">1 Hours</td>
-                        <td data-th="Duration">Name:Test name</br> Age: 28</td>
-                        <td data-th="Client Message"></td>
-                      </tr>
-                      <tr class="even">
-                        <td data-th="Dress Style">Sporty Chic</td>
-                        <td data-th="Location">Hotel Visit</td>
-                        <td data-th="Hotel"></td>
-                        <td data-th="Date of meeting">2017-1-24</td>
-                        <td data-th="Time of Date">3</td>
-                        <td data-th="Duration">1 Hours</td>
-                        <td data-th="Duration">Name:Test name</br> Age: 28</td>
-                        <td data-th="Client Message"></td>
-                      </tr>
-                      <tr class="odd">
-                        <td data-th="Dress Style">Sporty Chic</td>
-                        <td data-th="Location">Hotel Visit</td>
-                        <td data-th="Hotel"></td>
-                        <td data-th="Date of meeting">2017-1-24</td>
-                        <td data-th="Time of Date">3</td>
-                        <td data-th="Duration">1 Hours</td>
-                        <td data-th="Duration">Name:Test name</br> Age: 28</td>
-                        <td data-th="Client Message"></td>
-                      </tr>
+		   <?php
+$args = array(
+	'author' =>  $userdata->ID,
+	'numberposts' => -1,
+	'offset' => 0,
+	'category' => 0,
+	'orderby' => 'ASC',
+	'order' => 'ASC',
+	'include' => '',
+	'exclude' => '',
+	'meta_key' => '',
+	'meta_value' =>'',
+	'post_type' => 'booking_post_type',
+	'post_status' => 'publish',
+	'suppress_filters' => true
+);
+
+$recent_posts = wp_get_recent_posts( $args, ARRAY_A );
+$j=1;
+$input = array();
+foreach($recent_posts as $recent_posts)
+{
+	
+	$url = "http://localhost/public_html/newwp/lavish/payment-vip?pid=".$recent_posts['ID'];
+	//print_r($recent_posts['ID']);
+	
+	$class_name="";
+	
+	if($j%2==0)
+	{
+		$class_name="odd";
+	}
+	else
+	{
+		$class_name = "even";
+	}?>
+	
+	<tr class="<?php echo $class_name;?>">
+                        <td data-th="Model" class="dashbord-model-name">
+                          <h5><?php echo get_post_meta( $recent_posts['ID'], 'booking_forms_desired_mate', true ); ?></h5>
+                          <p> Age <?php echo get_post_meta( $recent_posts['ID'], 'extra_profile_fileds_age', true ); ?> </p>
+                          <a href="<?php echo $url;?>"> Book <?php echo get_post_meta( $recent_posts['ID'], 'booking_forms_desired_mate', true ); ?> again ?</a>
+                        </td>
+                        <td data-th="Dress style"><?php echo get_post_meta( $recent_posts['ID'], 'booking_forms_dress_style', true ); ?> </td>
+                        <td data-th="Location">Hotel visit</td>
+                        <td data-th="Hotel#"><?php echo get_post_meta( $recent_posts['ID'], 'booking_forms_hotel_room', true ); ?></td>
+                        <td data-th="Date of meeting"><?php echo get_post_meta( $recent_posts['ID'], 'booking_forms_date_of_meeting', true ); ?></td>
+                        <td data-th="Time of meeting"><?php echo get_post_meta( $recent_posts['ID'], 'booking_forms_time_of_meeting', true ); ?> </td>
+                        <td data-th="Duration"><?php echo get_post_meta( $recent_posts['ID'], 'booking_forms_duration', true ); ?></td>
+                        <td data-th="Message"> 
+                         <p> <?php echo get_post_meta( $recent_posts['ID'], 'booking_forms_what_is_your_desired_message_for_your_mate_', true ); ?></p>
+                        </td>
+                      </tr>  
+	
+	<?php
+	$j++;  
+}
+?>
+                
+                      
+                      
                  </table>
-                </div>
-                
-                
-                </div>
-             </div>   
-            </div><!-- .author -->
+                </div>          
+    </div>
+  </div>
+  <!-- #tab2 -->
+  <div class="tab_drawer_heading" rel="tab3"><h3> Payment Methods </h3></div>
+  <div id="tab3" class="tab_content">
+       <div class="payment-method-box">
+       <h3> Payment  Methods </h3>
+       <a href="#"> Add  payment method </a>
+      </div>
+       <div class="billing-payment-box">
+       <h3> Billing & Payment Processing </h3>
+      </div>
+       <div class="payment-info-box">
+        <table class="rwd-table" cellpadding="0" cellspacing="0">
+          <tr class="payment-info-box-top">
+            <th>Payment method</th>
+            <th>Actions</th>
+            <th>Actions</th>
+          </tr>
+          <tr>
+            <td data-th="Payment method"><span><img src="<?php echo esc_url( get_template_directory_uri() )?>/images/payment-icon.png" alt=""></span>Visa ending in 4650</td>
+            <td data-th="Actions"><a href="#">Edit | remove</a></td>
+            <td data-th="Actions">Primary </td>
+          </tr>
+          <tr class="payment-info-box-bottom">
+            <td data-th="Payment method"><span><img src="<?php echo esc_url( get_template_directory_uri() )?>/images/payment-icon.png" alt=""></span>Visa ending in 7474</td>
+            <td data-th="Actions"><a href="#">Edit | remove</a></td>
+            <td data-th="Actions">Set as primary</td>
+          </tr>
+        </table>
+        <p> We accept all major credit cards, as well as electronic transfers and cash payments
+We accept the following credit cards </p>
+         <img src="<?php echo esc_url( get_template_directory_uri() )?>/images/credit_grey.png" alt="">
+         <p> Extra 5 % handling fee </p>
+         <p class="payment-info-box-personal"> XE Personal Currency Assistant </p>
+      </div>
+  </div>
+  <!-- #tab3 -->
+  
+</div>
+    
+</div>
 
             <?php
         }
